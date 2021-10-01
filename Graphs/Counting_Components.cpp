2901ae8214950f3d211a79ea0cc42d95d24e724f
Sambhav Kaul <sambhav.kaul12@gmail.com>
2021-10-01 15:45:56 +0530

    Add counting components

---
+++ Graphs/Counting_Components.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+vector<int> ar[100001];
+int vis[100001];
+ 
+void DFS(int v)
+{
+    vis[v] = 1;
+ 
+    for (int child : ar[v])
+    {
+        if (vis[child] == 0)
+        {
+            DFS(child);
+        }
+    }
+}
+ 
+int main()
+{
+ 
+    int n, m, a, b;
+    cin >> n >> m;
+ 
+    for (int i = 1; i <= m; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+ 
+        ar[a].push_back(b);
+        ar[b].push_back(a);
+    }
+    int cc = 0;
+ 
+    for (int i = 1; i <= n; i++)
+    {
+        if (vis[i] == 0)
+        {
+            DFS(i);
+            cc++;
+        }
+    }
+    cout << cc << "\n";
+ 
+    return 0;
+}
